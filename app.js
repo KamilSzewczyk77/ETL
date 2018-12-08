b21393a49a54387f111370e28659bfcfeeabbf11
Kamil Szewczyk <k.szewczyk107@gmail.com>
2018-12-08 14:05:49 +0100

    connection with firebase

---
+++ app.js
@@ -0,0 +1,10 @@
+var config = {
+   apiKey: "AIzaSyBg0kSaoiku7sKgo0iQMg1aOS4h3x6iozQ",
+   authDomain: "hurt-danych-as-ks.firebaseapp.com",
+   databaseURL: "https://hurt-danych-as-ks.firebaseio.com/",
+   storageBucket: "hurt-danych-as-ks.appspot.com"
+ };
+ firebase.initializeApp(config);
+
+ // Get a reference to the database service
+ var database = firebase.database();
