903adb1e3ca1232899fde634316032606cdef5f3
Kamil Szewczyk <k.szewczyk107@gmail.com>
2018-12-08 17:04:45 +0100

    try 6x

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const preObject = document.getElementById('imdb');
 const ulList = document.getElementById('list');
 
-const dbRefObject = firebase.datebase().ref().child('imdb');
-const dbRefList = dbRefObject.child('TOP250')
+const dbRefObject = firebase.datebase().ref().child('TOP250');
+const dbRefList = dbRefObject.child('list')
 
 dbRefObject.on('value', snap => {
   preObject.innerText = JSON.stringify(snap.val(), null, 5);
