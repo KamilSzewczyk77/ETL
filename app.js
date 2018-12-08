0fdf1217373706ef8d4757cac74df1b0f5259952
Kamil Szewczyk <k.szewczyk107@gmail.com>
2018-12-08 16:10:47 +0100

    5x

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 const preObject = document.getElementById('imbd');
 const ulList = document.getElementById('list');
 
-const dbRefObject = firebase.datebase().ref().child('imbd');
-const dbRefList = dbRefObject.child('TOP250')
+const dbRefObject = firebase.datebase().ref().child('TOP250');
+const dbRefList = dbRefObject.child('2')
 
 dbRefObject.on('value', snap => {
-  preObject.innerText = JSON.stringify(snap.val(), null, 3);
+  preObject.innerText = JSON.stringify(snap.val());
 });
 
 dbRefList.on('child_added', snap => console.log(snap.val()));
