97d117e0c8c2125a59686042625d5ebfbddb2f20
Kamil Szewczyk <k.szewczyk107@gmail.com>
2018-12-08 16:19:49 +0100

    try

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-const preObject = document.getElementById('imbd');
+const preObject = document.getElementById('imdb');
 const ulList = document.getElementById('list');
 
-const dbRefObject = firebase.datebase().ref().child('TOP250');
-const dbRefList = dbRefObject.child('2')
+const dbRefObject = firebase.datebase().ref().child('imdb');
+const dbRefList = dbRefObject.child('TOP250')
 
 dbRefObject.on('value', snap => {
-  preObject.innerText = JSON.stringify(snap.val());
+  preObject.innerText = JSON.stringify(snap.val(), null, 5);
 });
 
 dbRefList.on('child_added', snap => console.log(snap.val()));
