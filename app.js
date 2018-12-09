cd2649601898c679ae0fed8ef3e04cc5141c9eb7
Kamil Szewczyk <k.szewczyk107@gmail.com>
2018-12-09 12:44:05 +0100

    dd

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-DatabaseReference zonesRef = FirebaseDatabase.getInstance().getReference("imdb");
-DatabaseReference zone1Ref = zonesRef.child("TOP250");
-DatabaseReference zone1NameRef = zone1Ref.child("Title");
-
-document.getElementById("list").innerHTML = 5 + 6;
+document.getElementById("list").innerHTML = "New text!";
