17e910c845d05db93f99d3424b3f7447627ebfb4
Kamil Szewczyk <k.szewczyk107@gmail.com>
2018-12-09 14:23:03 +0100

    Andrzej version

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+(function(){
 
-const preObject = document.getElementById('imbd');
-const ulList = document.getElementById('list');
+// Initialize Firebase
+   const config = {
+     apiKey: "AIzaSyBg0kSaoiku7sKgo0iQMg1aOS4h3x6iozQ",
+     authDomain: "hurt-danych-as-ks.firebaseapp.com",
+     databaseURL: "https://hurt-danych-as-ks.firebaseio.com",
+     //projectId: "hurt-danych-as-ks",
+     storageBucket: "hurt-danych-as-ks.appspot.com",
+     //messagingSenderId: "105336337375"
+   };
+   firebase.initializeApp(config);
+   
+   	// Get elements
+	const preObject = document.getElementById('imdb');
 
-const dbRefObject = firebase.datebase().ref().child('TOP250');
-const dbRefList = dbRefObject.child('3')
+	// Get references
+	const dbRefObject = firebase.database().ref().child('imdb');
 
-dbRefObject.on('value', snap => {
-  preObject.innerText = JSON.stringify(snap.val(), null, 6);
-});
+	// Sync object changes
+	dbRefObject.on('value', snap => console.log(snap.val()));
 
-dbRefList.on('child_added', snap => console.log(snap.val()));
+	//const ulList = document.getElementById('list');
+
+
+	//const dbRefList = dbRefObject.child('3');
+
+
+
+
+  	//preObject.innerText = JSON.stringify(snap.val(), null, 6);
+//});
+
+//dbRefList.on('child_added', snap => console.log(snap.val()));
+}());
