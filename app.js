db22b264a01836534bd9d2ca8ebf63ae1990c60e
Kamil Szewczyk <k.szewczyk107@gmail.com>
2018-12-09 14:13:21 +0100

    try ..

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-document.getElementById("list").innerHTML = "New text!";
+
+const preObject = document.getElementById('imbd');
+const ulList = document.getElementById('list');
+
+const dbRefObject = firebase.datebase().ref().child('TOP250');
+const dbRefList = dbRefObject.child('3')
+
+dbRefObject.on('value', snap => {
+  preObject.innerText = JSON.stringify(snap.val(), null, 6);
+});
+
+dbRefList.on('child_added', snap => console.log(snap.val()));
